8fe1c09115ef8857160b500d9e66f419fd028e2b
AbergerJonas <jonas.aberger@htl-saalfelden.at>
2024-01-16 09:03:47 +0100

    Added Static Game-Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 
 function App() {
   
-
   return (
-    <>
-    </>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+                <span className="player-name">Player 2</span>
+                <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+        <ol>
+            GAME BOARD
+          </ol>
+      </div>
+    </main>
   )
 }
 
